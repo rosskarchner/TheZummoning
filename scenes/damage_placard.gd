a68bb7ad03116c2a102456eeca3cabb7bf34edeb
Ross Karchner <ross@karchner.com>
2024-05-01 06:46:14 -0400

    sync up before major changes

---
+++ scenes/damage_placard.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var is_dead = false
+
+@onready var death_placard_scene=preload("res://scenes/death_placard.tscn")
+
+func queue_free_or_show_death():
+	if is_dead:
+		queue_free()
+	else:
+		$ColorRect.queue_free()
+		$Label.queue_free()
+		$AnimationPlayer.play("show_death")
+func queue_free_parent():
+	get_parent().queue_free()
